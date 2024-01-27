135368237efa6ae7b4491a82e49c53ce60c87d2a
AdityaShelke29 <adityashelke2014@gmail.com>
2024-01-27 16:27:09 -0500

    some admin stuff

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,6 +2,9 @@ import ReactDOM from 'react-dom/client';
 import Landing from './pages/Landing.tsx';
 import Demo from './Demo.tsx';
 import Dashboard from './pages/Dashboard.tsx';
+import AdminDashboard from './pages/AdminDashboard.tsx';
+import AdminTrends from './pages/AdminTrends.tsx'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Auth0Provider } from '@auth0/auth0-react';
@@ -23,6 +26,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <Route path="/" element={<Landing />} />
             <Route path="/dashboard-test" element={<Demo />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/admin-dashboard" element = {<AdminDashboard />} />
+            <Route path="/admin-dashboard/trends" element = {<AdminTrends/>} />
           </Routes>
       </BrowserRouter>
     </QueryClientProvider>
