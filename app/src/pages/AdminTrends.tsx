562d313c1d455016d873cc139912db5eb1456e16
AdityaShelke29 <adityashelke2014@gmail.com>
2024-01-27 22:33:58 -0500

    addition of marker showing

--- app/src/pages/AdminTrends.tsx
+++ app/src/pages/AdminTrends.tsx
@@ -5,14 +5,25 @@ import SideBar from '../components/trends/SideBar'
 // Replace and Remove
 import TrendForm from '../components/trends/TrendForm'
 
+import {useState} from 'react'
+
 const AdminTrends = () => {
+
+  const [lat, setLat] = useState(0); 
+  const [long, setLong] = useState(0); 
+
+  const [clickedMarkers, setClickedMarkers] = useState([]); 
+
+
   return (
     <div className = "admin-trends">
         <AdminNavBar email = "adityashelke2014@gmail.com" />
 
         <div className="content">
             <SideBar />
-            <TrendMap name = {"minimap"}/>
+            <TrendMap name = {"minimap"} lat = {lat} setLat = {setLat} 
+                      long = {long} setLong = {setLong} clickedMarkers = {clickedMarkers} 
+                      setClickedMarkers = {setClickedMarkers} />
             <TrendForm />
         </div>
     </div>
